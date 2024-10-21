f74599a91ff3d2463ab96481ea27d9e5f64c641b
Nickflanagn24 <lpao26ad@students.codeinstitute.net>
2024-10-21 18:34:48 +0000

    add function structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,32 @@
-console.log("Connected!")
+function rungame() {
+
+}
+
+function  checkAnswer() {
+
+}
+
+function CalculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+} 
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMulitiplyQuestion() {
+
+}
+
